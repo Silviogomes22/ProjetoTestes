df58ccfaa8bd8f6c7f44ad3517d69fcc695d8e78
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2022-11-21 22:47:15 -0300

    Classe para importar CSV

--- repositories/book_repositories.py
+++ repositories/book_repositories.py
@@ -1,8 +1,16 @@
 from entities.book import Book
+from CsvExtract import CsvExtract
 
 class BookRepositories:
     def __init__(self) -> None:
         self.list_books: list[Book] = []
+
+    def set_books(self, file_book : list):
+        for book in file_book[1:]:
+            list_book = book.split(";")
+            book = Book(int(list_book[0]), list_book[1], list_book[2], list_book[3],
+                        list_book[4], CsvExtract.format_str_price_to_float(list_book[5]))
+            self.list_books.append(book)
 
     def verif_if_book_exists(self, book_id: int) -> bool:
         for book in self.list_books:
